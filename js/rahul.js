30e23ceb2c7483aa2364add6355b554fbd52beef
Rahul Pandya <rahul25796@gmail.com>
2019-12-09 23:00:45 +0530

    added todo - complete transition added remove functionality

--- js/rahul.js
+++ js/rahul.js
@@ -31,3 +31,45 @@ darkModeToggle.addEventListener("click", () => {
     disableDarkMode();
   }
 });
+
+var getCardId = function(event) {
+  // get cardId of this item
+  var card = event.path.filter(element => {
+    return element.className === "grid-item card";
+  });
+  return card[0].id;
+};
+
+var removeThisItem = function(event) {
+  // get itemset of this item
+  var itemset = event.path.filter(element => {
+    return element.className === "item-set";
+  });
+  // remove this item
+  itemset[0].remove();
+};
+
+var completeThisItem = function(event) {
+  // get itemset of this item
+  var itemset = event.path.filter(element => {
+    return element.className === "item-set";
+  });
+  // get the text to add to complete text
+  var text = itemset[0].innerText;
+  // remove this item
+  itemset[0].remove();
+
+  // html for completed item
+  var html =
+    '<div class="item-set"><label class="label">' +
+    text +
+    '</label><div class="item-action"><div class="cross"><i class="fa fa-times" onclick="removeThisItem(event)"></i></div></div></div>';
+
+  // get completed element
+  var cardId = getCardId(event);
+  var completedItems = document
+    .querySelector("#" + cardId)
+    .querySelector(".completed-items");
+
+  completedItems.insertAdjacentHTML("beforeend", html);
+};
